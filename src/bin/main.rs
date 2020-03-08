a1fff044cf2f8217fe0c142cbe6c8afa77f3e86e
wilypomegranate <wilypomegranate@users.noreply.github.com>
2020-03-08 13:50:45 -0400

    Incorrect address for APB2.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -5,48 +5,29 @@
 //! Note: Without additional hardware, PC13 should not be used to drive an LED, see page 5.1.2 of
 //! the reference manual for an explanation. This is not an issue on the blue pill.
 
-#![deny(unsafe_code)]
 #![no_std]
 #![no_main]
 
+// makes `panic!` print messages to the host stderr using semihosting
+// extern crate panic_semihosting;
+
 use panic_halt as _;
 
-use nb::block;
-
 use cortex_m_rt::entry;
-use embedded_hal::digital::v2::OutputPin;
-use stm32f1xx_hal::{pac, prelude::*, timer::Timer};
+use cortex_m_semihosting::hprintln;
 
 #[entry]
 fn main() -> ! {
     // Get access to the core peripherals from the cortex-m crate
-    let cp = cortex_m::Peripherals::take().unwrap();
-    // Get access to the device specific peripherals from the peripheral access crate
-    let dp = pac::Peripherals::take().unwrap();
 
-    // Take ownership over the raw flash and rcc devices and convert them into the corresponding
-    // HAL structs
-    let mut flash = dp.FLASH.constrain();
-    let mut rcc = dp.RCC.constrain();
+    hprintln!("Hello, world!").unwrap();
 
-    // Freeze the configuration of all the clocks in the system and store the frozen frequencies in
-    // `clocks`
-    let clocks = rcc.cfgr.freeze(&mut flash.acr);
-
-    // Acquire the GPIOC peripheral
-    let mut gpioc = dp.GPIOC.split(&mut rcc.apb2);
-
-    // Configure gpio C pin 13 as a push-pull output. The `crh` register is passed to the function
-    // in order to configure the port. For pins 0-7, crl should be passed instead.
-    let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
-    // Configure the syst timer to trigger an update every second
-    let mut timer = Timer::syst(cp.SYST, &clocks).start_count_down(1.hz());
-
-    // Wait for the timer to trigger an update and change the state of the LED
-    loop {
-        block!(timer.wait()).unwrap();
-        led.set_high().unwrap();
-        block!(timer.wait()).unwrap();
-        led.set_low().unwrap();
+    // Enable clock for GPIOC.
+    let rcc_apb2enr = &mut (0x18) as *mut i32;
+    unsafe {
+        let cur_val = core::ptr::read_volatile(rcc_apb2enr);
+        core::ptr::write_volatile(rcc_apb2enr, cur_val | 0x10);
     }
+
+    loop {}
 }
